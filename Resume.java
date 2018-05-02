4ccc17839c44bf8f365948cc783c208eb5496fc8
parkhi05 <bhatnagarparkhi05@gmail.com>
2018-05-02 12:19:07 +0530

    Day1

---
+++ Resume.java
@@ -0,0 +1,46 @@
+class Resume
+{
+  public static void main(String args[])
+  {
+ 
+  System.out.println("\t \t \t\t\t\t Parkhi Bhatnagar");
+  System.out.println("\t \t \t\t\t\t +91-9650775683");
+  System.out.println("\t \t \t\t\t\t bhatnagarparkhi05@gmail.com");
+  System.out.println("\t Educational Background : ");
+  System.out.println("\t Degree \t\t Institute \t\t Marks \t\t Year");
+  System.out.println("\t B-Tech(Cse) \t\t NIIT University \t 7 CGPA \t 2017");
+  System.out.println("\t X11 \t\t\t DPSG \t\t\t 72% \t\t 2013");
+  System.out.println("\t X \t\t\t DPSG \t\t\t 78% \t\t 2011");
+  System.out.println("\n\t");
+  System.out.println("\t PROFESSIONAL EXPERIENCE:");
+  System.out.println("\t CREDEXT TECHNOLOGIES - Gurugram, Haryana");
+  System.out.println("\t Software Developer \t");
+  System.out.println("\t 1. Developed a Face Recognition System for an easy and secure way to authenticate \n\t employees of an organization.");
+  System.out.println("\t 2. Facilitated implementation of an Image Enhancement scheme (CLAHE) to improve facial recognition");
+  System.out.println("\n\t");
+  System.out.println("\t Sterling and wilson Pvt Ltd");
+  System.out.println("\t Working as a management trainee in IT department.");
+  System.out.println("\n\t");
+  System.out.println("\t Projects");
+  System.out.println("\t 1. Online Examination System : ");
+  System.out.println("\t Worked on various phases of SDLC from requirement to development phase to create a website using HTML ,  PHP and Javascript");
+  System.out.println("\t 2. Library Database Management  : ");
+  System.out.println("\t Obtained records from library and organized them into a database using MYSQL Workbench ");
+  System.out.println("\t 3. Azure workshop :");
+  System.out.println("\t Worked closely with MIC trainers to develop a windows application");
+  System.out.println("\n\t");
+  System.out.println("\t Technical Skills");
+  System.out.println("\t 1. Languages : Java, HTML5,CSS3, Javascript, C, C++, Python, SQL,PHP \n\t 2. Tools : MATLAB, Adobe Photoshop, WordPress , Microsoft Office (Excel, PowerPoint and Word) \n\t 3. Digital marketing skills:  Search Engine Optimization (SEO) ,  Social Media Marketing"); 
+  System.out.println("\n\t");
+  System.out.println("\t Strengths : \n\t 1. Ability to handle multiple tasks and projects simultaneously. \n\t 2. Good analytic and problem solving skills. \n\t 3. Hardworking and positive attitude . ");
+
+
+
+
+
+
+
+
+}
+
+}
